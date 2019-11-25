b6b414949ebaa92300d22a738fccd9be35793aab
Nguyễn Dũng <quocdung1612@gmail.com>
2019-11-25 22:33:10 +0700

    appBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 import * as serviceWorker from './serviceWorker';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+<Router history={hist}>
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  </Router>,
+ document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
